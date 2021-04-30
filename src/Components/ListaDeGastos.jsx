bc89aa1647546ab4cd03061344a85beb0bc81316
SnowMoon272 <CastielAltair0027@outlook.com>
2021-04-29 21:52:55 -0500

    Preparandome para experimentar

--- src/Components/ListaDeGastos.jsx
+++ src/Components/ListaDeGastos.jsx
@@ -3,24 +3,51 @@ import { Helmet } from "react-helmet";
 import { Header, Titulo } from "../Tools/Header";
 import BtnRegresar from "../Tools/BtnRegresar";
 import BarraTotalGastado from "./BarraTotalGastado";
-// import { useAuth } from "../contextos/AuthContext";
+import useObtenerGastos from "../Hooks/useObtenerGastos";
+import {
+  Lista,
+  ElementoLista,
+  ListaDeCategorias,
+  ElementoListaCategorias,
+  Categoria,
+  Descripcion,
+  Valor,
+  Fecha,
+  ContenedorBotones,
+  BotonAccion,
+  BotonCargarMas,
+  ContenedorBotonCentral,
+  ContenedorSubtitulo,
+  Subtitulo,
+} from "../Tools/ElementosDeLista";
 
-function ListaDeGastos() {
-  // const { usuario } = useAuth();
-  // console.log(usuario);
+const ListaDeGastos = () => {
+  const [gastos] = useObtenerGastos();
+  console.log("Loas gastos son:");
+  console.log(gastos);
+
   return (
     <>
       <Helmet>
         <title>Lista de Gastos</title>
       </Helmet>
+
       <Header>
         <BtnRegresar ruta="/" />
         <Titulo>Lista de Gastos</Titulo>
       </Header>
 
+      {/* <Lista>
+        {gastos.map((gasto) => {
+          return (
+            <ElementoLista key={gasto.id}>{gasto.Descripcion}</ElementoLista>
+          );
+        })}
+      </Lista> */}
+
       <BarraTotalGastado />
     </>
   );
-}
+};
 
 export default ListaDeGastos;
---
+++ src/Hooks/useObtenerGastos.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/firebaseConfig";
+import { useAuth } from "../contextos/AuthContext";
+
+const useObtenerGastos = () => {
+  const { usuario } = useAuth();
+  const [gastos, cambiarGastos] = useState([]);
+
+  useEffect(() => {
+    const unsuscribe = db
+      .collection("gastos")
+      .where("Id_User", "==", usuario.uid)
+      .orderBy("fecha", "desc")
+      .limit(10)
+      .onSnapshot((snapshot) => {
+        cambiarGastos(
+          snapshot.docs.map((gasto) => {
+            return { ...gasto.data(), id: gasto.id };
+          }),
+        );
+      });
+    return unsuscribe;
+  }, [usuario]);
+
+  return [gastos];
+};
+
+export default useObtenerGastos;
